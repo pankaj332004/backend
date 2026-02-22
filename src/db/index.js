0232188881d993c4f67d5e6033e4530b8a6847a9
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2026-02-22 17:30:49 +0530

    mongodb connection establish

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n Connected to MongoDB !! DB HOST
+            : ${connectionInstance.connection.host}\n`);
+    }catch (error){
+        console.log("MONGODB connection failed",error);
+        process.exit(1);    
+    }
+}
+
+export default connectDB;
